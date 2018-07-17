a7363c7536972d6c1d409ce4c706883a5e6bd2cb
Manuel Rauber <manuel.rauber@thinktecture.com>
2018-07-17 08:43:14 +0200

    test: ChaincodeSettings

---
+++ src/Chaincode.NET/Chaincode.NET.Test/Settings/ChaincodeSettingsTest.cs
@@ -0,0 +1,30 @@
+using Chaincode.NET.Settings;
+using FluentAssertions;
+using Xunit;
+
+namespace Chaincode.NET.Test.Settings
+{
+    public class ChaincodeSettingsTest
+    {
+        [Fact]
+        public void PeerAddress_maps_to_external_CORE_PEER_ADDRESS()
+        {
+            var sut = new ChaincodeSettings {CORE_PEER_ADDRESS = "foobar"};
+            sut.PeerAddress.Should().Be("foobar");
+        }
+
+        [Fact]
+        public void ChaincodeIdName_maps_to_external_CORE_CHAINCODE_ID_NAME()
+        {
+            var sut = new ChaincodeSettings() {CORE_CHAINCODE_ID_NAME = "foobar"};
+            sut.ChaincodeIdName.Should().Be("foobar");
+        }
+
+        [Fact]
+        public void PeerId_maps_to_external_CORE_PEER_ID()
+        {
+            var sut = new ChaincodeSettings() {CORE_PEER_ID = "foobar"};
+            sut.PeerId.Should().Be("foobar");
+        }
+    }
+}
