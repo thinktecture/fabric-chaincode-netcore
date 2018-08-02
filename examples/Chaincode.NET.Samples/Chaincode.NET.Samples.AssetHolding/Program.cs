31fd9886589873e29e0861ee163dbff1d9424a42
Manuel Rauber <manuel.rauber@thinktecture.com>
2018-08-02 18:15:42 +0200

    chore: moved samples into examples folder

---
+++ examples/Chaincode.NET.Samples/Chaincode.NET.Samples.AssetHolding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chaincode.NET.Samples.AssetHolding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
