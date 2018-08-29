67d45a9f6f7102b687f29c5154e885df2473a97e
Manuel Rauber <manuel.rauber@thinktecture.com>
2018-08-29 11:31:41 +0200

    fix: meta contract

--- src/Chaincode/Chaincode/Contract/MetaContract.cs
+++ src/Chaincode/Chaincode/Contract/MetaContract.cs
@@ -1,3 +1,8 @@
+using System.Threading.Tasks;
+using Google.Protobuf;
+using Newtonsoft.Json;
+using Thinktecture.HyperledgerFabric.Chaincode.Extensions;
+
 namespace Thinktecture.HyperledgerFabric.Chaincode.Contract
 {
     public class MetaContract : ContractBase
@@ -6,5 +11,10 @@ namespace Thinktecture.HyperledgerFabric.Chaincode.Contract
             : base("org.hyperledger.fabric", null)
         {
         }
+
+        public Task<ByteString> GetMetadata(IContractContext context)
+        {
+            return Task.FromResult(JsonConvert.SerializeObject(Metadata).ToByteString());
+        } 
     }
 }
