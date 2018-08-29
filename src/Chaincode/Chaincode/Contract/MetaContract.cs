85caf1823d4dbea64bb2ceb17a25a278d544d77e
Manuel Rauber <manuel.rauber@thinktecture.com>
2018-08-29 11:11:16 +0200

    fix: meta contract

--- src/Chaincode/Chaincode/Contract/MetaContract.cs
+++ src/Chaincode/Chaincode/Contract/MetaContract.cs
@@ -1,11 +1,9 @@
-using System.Collections.Generic;
-
 namespace Thinktecture.HyperledgerFabric.Chaincode.Contract
 {
     public class MetaContract : ContractBase
     {
-        public MetaContract(string @namespace, IDictionary<string, string> metadata = null)
-            : base(@namespace, metadata)
+        public MetaContract()
+            : base("org.hyperledger.fabric", null)
         {
         }
     }
