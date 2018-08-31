2a9eb196263a6e223576ba25e8c601ad70d33bec
Manuel Rauber <manuel.rauber@thinktecture.com>
2018-08-31 12:14:43 +0200

    chore: Exclude ChaincodesupportClientFactory as well

--- src/Chaincode/Chaincode/Handler/ChaincodeSupportClientFactory.cs
+++ src/Chaincode/Chaincode/Handler/ChaincodeSupportClientFactory.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using Grpc.Core;
 using Protos;
 
 namespace Thinktecture.HyperledgerFabric.Chaincode.Handler
 {
     /// <inheritdoc />
+    [ExcludeFromCodeCoverage] 
     public class ChaincodeSupportClientFactory : IChaincodeSupportClientFactory
     {
         public ChaincodeSupport.ChaincodeSupportClient Create(Channel channel)
