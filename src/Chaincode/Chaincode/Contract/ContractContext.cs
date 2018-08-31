2d50b90798dda1162d8903ff024c0777e7c3aca7
Manuel Rauber <manuel.rauber@thinktecture.com>
2018-08-31 12:05:22 +0200

    chore: excluding ContractContext from Coverage

--- src/Chaincode/Chaincode/Contract/ContractContext.cs
+++ src/Chaincode/Chaincode/Contract/ContractContext.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
 using Thinktecture.HyperledgerFabric.Chaincode.Chaincode;
 
 namespace Thinktecture.HyperledgerFabric.Chaincode.Contract
 {
+    [ExcludeFromCodeCoverage]
     public class ContractContext : IContractContext
     {
         public IChaincodeStub Stub { get; set; }
